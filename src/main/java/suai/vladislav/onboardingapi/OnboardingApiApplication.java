ea62bb5cf9a8948934c4b921b01df55ea27f8b60
ANANAzZzZz <vladscooltrol@gmail.com>
2025-02-18 11:05:08 +0300

    initial commit

---
+++ src/main/java/suai/vladislav/onboardingapi/OnboardingApiApplication.java
@@ -0,0 +1,13 @@
+package suai.vladislav.onboardingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnboardingApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnboardingApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/suai/vladislav/onboardingapi/OnboardingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package suai.vladislav.onboardingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnboardingApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
