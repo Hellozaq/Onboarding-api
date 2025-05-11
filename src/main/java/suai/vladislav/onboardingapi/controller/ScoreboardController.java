7d8a14c22c3efd1126f774bbc1537e1126892e25
ANANAzZzZz <vladscooltrol@gmail.com>
2025-05-11 17:04:31 +0300

    add CRUD operations to scoreboard; add some api-requests template

--- src/main/java/suai/vladislav/onboardingapi/controller/ScoreboardController.java
+++ src/main/java/suai/vladislav/onboardingapi/controller/ScoreboardController.java
@@ -1,8 +1,16 @@
 package suai.vladislav.onboardingapi.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import suai.vladislav.onboardingapi.dto.ScoreboardDto;
 import suai.vladislav.onboardingapi.service.interfaces.ScoreboardService;
@@ -17,7 +25,29 @@ public class ScoreboardController {
     private final ScoreboardService scoreboardService;
 
     @GetMapping
-    public List<ScoreboardDto> getScoreboard() {
+    public List<ScoreboardDto> getScoreboards() {
         return scoreboardService.getScoreboard();
+    }
+
+    @GetMapping("/{scoreboardId}")
+    public ScoreboardDto getScoreboard(@PathVariable("scoreboardId") Long scoreboardId) {
+        return scoreboardService.getScoreboardById(scoreboardId);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ScoreboardDto addScoreboard(@Validated @RequestBody ScoreboardDto scoreboardDto) {
+        return scoreboardService.addScoreboard(scoreboardDto);
+    }
+
+    @PutMapping
+    public ScoreboardDto updateScoreboard(@Validated @RequestBody ScoreboardDto scoreboardDto) {
+        return scoreboardService.updateScoreboard(scoreboardDto);
+    }
+
+    @DeleteMapping("/{scoreboardId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteScoreboard(@Validated @PathVariable("scoreboardId") Long scoreboardId) {
+        scoreboardService.deleteScoreboard(scoreboardId);
     }
 }
--- src/main/java/suai/vladislav/onboardingapi/service/interfaces/ScoreboardService.java
+++ src/main/java/suai/vladislav/onboardingapi/service/interfaces/ScoreboardService.java
@@ -6,4 +6,12 @@ import java.util.List;
 
 public interface ScoreboardService {
     List<ScoreboardDto> getScoreboard();
+
+    ScoreboardDto getScoreboardById(Long id);
+
+    ScoreboardDto addScoreboard(ScoreboardDto scoreboardDto);
+
+    ScoreboardDto updateScoreboard(ScoreboardDto scoreboardDto);
+
+    void deleteScoreboard(Long id);
 }
